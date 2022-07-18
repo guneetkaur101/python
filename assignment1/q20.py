a1a7605c353e7d721126ffc101b2486eb43e4182
guneetkaur101 <guneetsidhu40@gmail.com>
2022-07-18 14:57:38 +0530

    First commit

---
+++ assignment1/q20.py
@@ -0,0 +1,8 @@
+# Write a Python function to reverses a string if it&#39;s length is a multiple of 4.
+def reverse_string(str1):
+    if len(str1) % 4 == 0:
+       return ''.join(reversed(str1))
+    return str1
+
+print(reverse_string('abcd'))
+print(reverse_string('python'))
---
+++ assignment1/q23.py
@@ -0,0 +1,3 @@
+#Write a Python program to check whether a string starts with specified characters.
+string = "w3resource.com"
+print(string.startswith("w3r"))
